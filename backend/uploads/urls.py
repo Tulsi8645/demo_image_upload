6b7b87a9028a9aba9b13b3afe03ddf23274224ed
Tulce <tulsi.gautam0000@gmail.com>
2024-06-22 10:16:10 +0545

    demo_image_upload

---
+++ backend/uploads/urls.py
@@ -0,0 +1,7 @@
+# uploads/urls.py
+from django.urls import path
+from .views import ImageUploadView
+
+urlpatterns = [
+    path('upload/', ImageUploadView.as_view(), name='image-upload'),
+]
---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ImageUpload from './components/ImageUploadOrigin';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+               
+            </header>
+            <main>
+                <ImageUpload />
+            </main>
+        </div>
+    );
+}
+
+export default App;
+
